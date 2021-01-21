5916f0b3ac1bff534534e0c70ffc76a2e27eaf8a
SahilRajpal-hub <sahilrajpal.31586@gmail.com>
2021-01-21 19:06:54 +0530

    navbars and landing page implemented

--- client/src/App.js
+++ client/src/App.js
@@ -4,13 +4,19 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import Navbar from './Components/Layouts/Navbar'
 import Landing from './Components/Layouts/Landing'
 import Login from './Components/auth/Login'
-import Register from './Components/auth/Login'
+import Register from './Components/auth/Register'
 
 const App = () => (
   <Router>
     <Fragment>
       <Navbar />
       <Route exact path='/' component={Landing} />
+      <section className='container'>
+        <Switch>
+          <Route exact path='/login' component={Login} />
+          <Route exact path='/register' component={Register} />
+        </Switch>
+      </section>
     </Fragment>
   </Router>
 )
